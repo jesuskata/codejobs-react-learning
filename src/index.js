6a77ab95e77d14f65fb19051a5dd2ef085458b1c
jesuskata <rzarate.alex@gmail.com>
2018-08-03 11:10:10 -0500

    Este es el primer repositorio e incluye las primeras 5 clases del curso Aprendiendo ReactJs con CodeJobs.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Dependencies
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Routes
+import AppRoutes from './routes';
+
+// Assets
+import './index.css';
+
+render(
+  <Router>
+    <AppRoutes />
+  </Router>,
+  document.getElementById('root')
+);
